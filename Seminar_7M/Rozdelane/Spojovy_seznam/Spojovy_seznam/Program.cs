4a673f0af9480b1110e16cba187da75f53ee166b
Fimlar <filip@gaa.cz>
2024-10-16 15:53:35 +0200

    Úprava na hodině
    Přidání funkcí Add a Find

--- Seminar_7M/Rozdelane/Spojovy_seznam/Spojovy_seznam/Program.cs
+++ Seminar_7M/Rozdelane/Spojovy_seznam/Spojovy_seznam/Program.cs
@@ -10,6 +10,45 @@ namespace Spojovy_seznam
     {
         static void Main(string[] args)
         {
+            Node uzlik = new Node(8);
+            
+        }
+    }
+    class Node
+    {
+        public Node(int value)  //konstruktor
+        {
+            Value = value;
+        }
+        public int Value { get; }
+        public Node Next { get; set;  }
+    }
+    class LinkedList
+    {
+        public Node Head { get; set; }
+        public void Add(int value)  //přidat prvek do seznamu
+        {
+            if (Head == null)   //když je seznam prázdný
+            {
+                Head = new Node(value); //vyrobím nový prvek seznamu
+            }   
+            else
+            {
+                Node newNode = new Node(value);
+                newNode.Next = Head;
+                Head = newNode;
+            }
+        }
+        public bool Find(int value)
+        {
+            Node node = Head;
+            while (node != null)   //dokud nedojedeme na konec seznamu
+            {
+                if (node.Value == value)
+                    return true;
+                node = node.Next;
+            }
+            return false;
         }
     }
 }
