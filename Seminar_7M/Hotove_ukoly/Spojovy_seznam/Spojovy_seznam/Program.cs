d44b354b3b789176f0511a2f9caa66e8a0b3a554
Fimlar <filip@gaa.cz>
2024-10-23 15:54:57 +0200

    Spojový seznam přidání Print a BubbleSort

--- Seminar_7M/Hotove_ukoly/Spojovy_seznam/Spojovy_seznam/Program.cs
+++ Seminar_7M/Hotove_ukoly/Spojovy_seznam/Spojovy_seznam/Program.cs
@@ -12,6 +12,11 @@ namespace Spojovy_seznam
         {
             Node uzlik = new Node(8);          
             LinkedList list = new LinkedList();
+            list.PrintList();
+
+            list.BubbleSort(list);
+            list.PrintList();
+            Console.ReadLine();
         }
     }
     class Node
@@ -20,7 +25,7 @@ namespace Spojovy_seznam
         {
             Value = value;
         }
-        public int Value { get; }
+        public int Value { get; set;  }
         public Node Next { get; set;  }
     }
     class LinkedList
@@ -61,6 +66,75 @@ namespace Spojovy_seznam
                 node = node.Next;
             }
             return min;
+        }
+        public void PrintList()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Value);
+                Console.Write(' ');
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+        public int Len(LinkedList list)
+        {
+            Node node = Head;
+            int len = 0;
+            while (node != null)
+            {
+                len++;
+                node = node.Next;
+            }
+            return len;
+        }
+        public LinkedList Sort2(LinkedList list)
+        {
+            LinkedList result = new LinkedList();
+            int previous = 0;
+            Node current = Head;
+            int min = 0;
+            while (current != null)
+            {
+                while (current != null)
+                {
+                    if (previous < current.Value & current.Value < min)
+                    {
+                        min = current.Value;
+                    }
+                    current = current.Next;
+                }
+                result.Add(min);
+                previous = min;
+                
+            }
+            return result;
+        }
+        public LinkedList BubbleSort(LinkedList list)
+        {
+            Node current = Head;
+            Node previous = Head;
+            int bigger = 0;
+            int smaller = 0;
+            int len = list.Len(list);
+            for (int i = 0; i < len; i++)
+            {
+                while (current.Next != null)
+                {
+                    if (current.Value > current.Next.Value)
+                    {
+                        bigger = current.Value;
+                        smaller = current.Next.Value;
+                        current.Next.Value = bigger;
+                        current.Value = smaller;
+                    }
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+            
+            return list;
         }
     }
 }
