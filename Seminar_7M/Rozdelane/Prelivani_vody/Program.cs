c1afe7091a179fd11cd7b34c401672f68958afdc
Fimlar <filip@gaa.cz>
2024-11-20 15:53:49 +0100

    Vytvoření souboru Přelévání vody

---
+++ Seminar_7M/Rozdelane/Prelivani_vody/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prelivani_vody
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //načtení vstupu
+            string[] input = Console.ReadLine().Split(' ');
+            int max1 = int.Parse(input[0]);
+            int max2 = int.Parse(input[1]);
+            int goal1 = int.Parse(input[2]);
+            int goal2 = int.Parse(input[3]);
+
+        }
+    }
+}
