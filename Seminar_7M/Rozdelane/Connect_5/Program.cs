ed300cb6d0af15b60b0a361f29498b1006207984
Fimlar <filip@gaa.cz>
2025-02-26 15:49:37 +0100

    Connect 4 create

---
+++ Seminar_7M/Rozdelane/Connect_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static Tuple<int, int, string, string> Input()
+        {
+            Console.WriteLine("Zadej šířku hracího pole: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Zadej výšku hracího pole: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Zadej jméno prvního hráče: ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Zadej jméno druhého hráče: ");
+            string name2 = Console.ReadLine();
+            return Tuple.Create(width, height, name1, name2);
+        }
+    }
+}
