9d79197fe852da4fdb45d29bee3b063b30af6684
Fimlar <filip@gaa.cz>
2025-04-30 15:41:50 +0200

    Výrazový strom hotovo

---
+++ Seminar_7M/Rozdelane/Vyrazovy_strom/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Vyrazovy_strom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressionTree<string> tree = new ExpressionTree<string>();
+            Console.WriteLine("Napiš příklad v Postfixu:");
+            string[] input = Console.ReadLine().Split();
+            tree.CreateFromPost(tree, input);
+            Console.WriteLine(tree.ShowInfix());
+            Console.WriteLine(tree.ShowPrefix());
+        }
+    }
+    class Node<T>
+    {
+        // public int Key { get; set; }
+        public T Value { get; set; }
+        public Node<T> LeftSon { get; set; }
+        public Node<T> RightSon { get; set; }
+        public Node<T> Parent { get; set; }
+
+        // konstruktor
+        public Node(T value)
+        {
+            // Key = key;
+            Value = value;
+            LeftSon = null; RightSon = null; Parent = null;
+        }
+    }
+    class ExpressionTree<T>
+    {
+        static Stack<Node<string>> stack = new Stack<Node<string>>();
+        public Node<string> Root { get; set; }
+
+        public ExpressionTree<T> CreateFromPost(ExpressionTree<T> tree, string[] input)
+        {
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (float.TryParse(input[i], NumberStyles.Float, CultureInfo.InvariantCulture, out float number))
+                {
+                    // Pokud najdu číslo
+                    Node<string>node = new Node<string>(input[i]);
+                    stack.Push(node);
+                }
+                    
+                else
+                {
+                    // Pokud najdu operátor
+                    Node<string> node = new Node<string>(input[i]);
+                    node.RightSon = stack.Pop();
+                    node.LeftSon = stack.Pop();
+                    stack.Push(node);
+                    if (i ==  input.Length - 1)
+                        Root = node;
+                }
+            }
+            return tree;
+        }
+
+        public string ShowInfix()
+        {
+            void _show(Node<string> node, StringBuilder nodes)
+            {
+                if (node != null)
+                {
+                    if (node.RightSon != null && node.LeftSon != null)
+                        nodes.Append("(");
+
+                    _show(node.LeftSon, nodes);
+                    nodes.Append(node.Value); //využijeme StringBuilder, abychom nevytvářely s každým dalším klíčem nový string => časově i paměťově daleko méně náročné než += u stringu
+                    
+                    _show(node.RightSon, nodes);
+                    if (node.RightSon != null && node.LeftSon != null)
+                        nodes.Append(")");
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            _show(Root, sb);
+            sb.Remove(0, 1);
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString(); // výpis ponecháme jednou naráz v Mainu, WriteLine do konzole je časově drahá operace
+        }
+
+        public string ShowPrefix()
+        {
+            void _show(Node<string> node, StringBuilder nodes)
+            {
+                if (node != null)
+                {
+                    nodes.Append(node.Value); //využijeme StringBuilder, abychom nevytvářely s každým dalším klíčem nový string => časově i paměťově daleko méně náročné než += u stringu
+                    nodes.Append(" ");
+                    _show(node.LeftSon, nodes);
+                    _show(node.RightSon, nodes);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            _show(Root, sb);
+            return sb.ToString(); // výpis ponecháme jednou naráz v Mainu, WriteLine do konzole je časově drahá operace
+        }
+    }
+}
---
+++ Seminar_7M/Rozdelane/Vyrazovy_strom/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Vyrazovy_strom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GCHD")]
+[assembly: AssemblyProduct("Vyrazovy_strom")]
+[assembly: AssemblyCopyright("Copyright © GCHD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("281ab9fb-9dd6-4845-bf93-e8e4d4f5de35")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
