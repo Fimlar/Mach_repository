892a5e07ae043b401a1b655044c68fc8213e319c
Fimlar <filip@gaa.cz>
2025-06-08 22:36:59 +0200

    Connect4 do 7M

---
+++ 7M/Hotove_ukoly/Connect4/AI.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Connect4
+{
+    // Tento kód je dělaný podle tutoriálu na vytvoření Connect 4 solveru viz http://blog.gamesolver.org/solving-connect-four/01-introduction/
+    // Funkce Negamax a AplhaBeta jsou přímo upravené jeho funkce, ostatní je více či méně inspirované
+    // Vše v Program.cs je zcela originální
+
+
+    /// <summary>
+    /// Třída, která uchovává hrací pole
+    /// </summary>
+    public class Position
+    {
+        public bool SetPosition(string seq)
+        {
+            for (int i = 0; i < seq.Length; i++)
+            {
+                int col = seq[i] - '1';
+                if (col < 0 || col >= this.WIDTH || !CanPlay(col))      // Kontrolujeme, jestli lze daný sloupec zahrát
+                    return false;
+                Play(col);
+            }
+            moves = seq.Length;         // Nastavujeme počet zahraných tahů na délku vstupu
+            return true;
+        }
+
+
+        /// <summary>
+        /// Vlastnosti HEIGHT, WIDTH a winCount potřebuju vždy získat, ovšem nikdy je nemusím jinde měnit
+        /// </summary>
+        public int HEIGHT { get; private set; }
+        public int WIDTH { get; private set; }
+        public int winCount { get; set; }
+
+        private int[,] board;       // Hrací pole   
+        private int[] height;       // Pole s výškou každého sloupce
+        private int moves = 0;      // Počet tahů od začátku hry
+
+
+        /// <summary>
+        /// Konstruktor třídy Position
+        /// </summary>
+        /// <param name="hEIGHT">Výška hracího pole</param>
+        /// <param name="wIDTH">Šířka hracího pole</param>
+        /// <param name="winCount">Počet vítězných kamenů</param>
+        public Position(int hEIGHT, int wIDTH, int winCount)
+        {
+            HEIGHT = hEIGHT;
+            WIDTH = wIDTH;
+            this.winCount = winCount;
+            this.board = new int[wIDTH, hEIGHT];
+            this.height = new int[wIDTH];
+        }
+
+
+        /// <summary>
+        /// Konstruktor, který zkopíruje zadanou pozici
+        /// </summary>
+        /// <param name="P">Zadaná pozice</param>
+        public Position(Position P)                             // Není zde potřeba, ovšem hodí se lol
+        {
+            WIDTH = P.WIDTH;
+            HEIGHT = P.HEIGHT;
+            winCount = P.winCount;
+
+            board = new int[WIDTH, HEIGHT];
+            height = new int[WIDTH];
+            moves = P.moves;
+
+            for (int i = 0; i < HEIGHT; i++)
+                for (int j = 0; j < WIDTH; j++)
+                    board[j, i] = P.board[j, i];
+            for (int i = 0; i < WIDTH; i++)
+                height[i] = P.height[i];
+        }
+
+
+        /// <summary>
+        /// Vytiskne hrací pole
+        /// </summary>
+        public void PrintBoard()
+        {
+            for (int i = 0; i < HEIGHT; i++)
+            {
+                for (int j = 0; j < WIDTH; j++)
+                {
+                    if (board[j, i] == 0)
+                        Console.Write("[ ] ");  // Prázdná buňka
+                    else if (board[j, i] == 1)
+                        Console.Write("[X] ");  // Tah hráče 1
+                    else
+                        Console.Write("[O] ");  // Tah hráče 2
+                }
+                Console.WriteLine();  // Nový řádek po každém řádku hracího pole
+            }
+            Console.WriteLine();  // Prázdný řádek mezi jednotlivými výtisky hracího pole
+        }
+
+
+        /// <summary>
+        /// Zkontoluje jestli není vybraný sloupec plný
+        /// </summary>
+        /// <param name="col">Číslo sloupce, do kterého chceme hodit kámen</param>
+        /// <returns>true: sem kámen hodit můžeme; false: sloupec je plný</returns>
+        public bool CanPlay(int col)
+        {
+            return height[col] < HEIGHT;
+        }
+
+
+        /// <summary>
+        /// Vrací počet zahraných tahů
+        /// </summary>
+        /// <returns>Počet zahraných tahů</returns>
+        public int NbMoves()
+        {
+            return moves;
+        }
+
+
+        /// <summary>
+        /// Zahraje kámen do vybraného sloupce
+        /// </summary>
+        /// <param name="col">Sloupec, do kterého hrajeme kámen</param>
+        public void Play(int col)
+        {
+            board[col, HEIGHT - height[col]-1] = 1 + moves % 2;     // Položíme kámen na jeho pozici
+            height[col]++;                                          // Zvětšíme výšku v tomto sloupci
+            moves++;                                                // Zvětšíme počet zahraných tahů
+        }
+
+
+        /// <summary>
+        /// Kontroluje jestli když zahraji kámen do zadaného sloupce, tak někdo vyhraje
+        /// </summary>
+        /// <param name="col">Sloupec, do kterého házíme kámen</param>
+        /// <param name="currentPlayer">Hráč, pro kterého kontrolujeme výhru</param>
+        /// <returns>true: hráč vyhraje; false: hráč nevyhraje</returns>
+        public bool IsWinningMove(int col, int currentPlayer)
+        {
+            int y = HEIGHT - height[col] - 1;
+            return CheckDirection(col, y, 1, 0, currentPlayer) || CheckDirection(col, y, 0, 1, currentPlayer) || CheckDirection(col, y, 1, 1, currentPlayer) || CheckDirection(col, y, -1, 1, currentPlayer);
+        }
+
+
+        /// <summary>
+        /// Kontroluje pro zadaný směr výhru hráče
+        /// </summary>
+        /// <param name="col">Sloupec, do kterého házíme kámen</param>
+        /// <param name="row">Řada, do které kámen dopadne</param>
+        /// <param name="dRow">Směr v řádku</param>
+        /// <param name="dCol">Směr ve sloupci</param>
+        /// <param name="currentPlayer">Hráč, pro kterého kontrolujeme výhru</param>
+        /// <returns>true: v tomto směru je výhra; false: v tomto směru není výhra</returns>
+        public bool CheckDirection(int col, int row, int dRow, int dCol, int currentPlayer)
+        {
+            int count = 1; // Začínáme s 1, protože aktuální tah je už na pozici (col, row)
+
+            // Zkontrolujeme oba směry (pozitivní a negativní směr)
+            for (int j = -1; j < 2; j+=2)
+            {
+                for (int i = 1; i < winCount; i++)      // Počítáme od 1, protože pozice (col, row) už je zahrnuta
+                {
+                    int newRow = row + i * dRow * j;    // Nový řádek
+                    int newCol = col + i * dCol * j;    // Nový sloupec
+                    if (newRow < 0 || newRow >= HEIGHT || newCol < 0 || newCol >= WIDTH || board[newCol, newRow] != currentPlayer)
+                        break;      // Pokud je mimo hranice nebo hodnoty se liší, přerušíme kontrolu
+                    count++;        // Přidáme 1 k počtu propojených žetonů
+                }
+            }
+            return count >= winCount;       // Pokud je propojeno dostatečně žetonů, hráč vyhrál
+        }
+    }
+
+    public class Solver
+    {
+        private int[] columnOrder;      // Pořadí, ve kterém se prozkoumávají sloupce
+        private int bestCol;
+
+        /// <summary>
+        /// Konstruktor třídy Solver; slouží primárně k nastavení priority sloupců
+        /// </summary>
+        /// <param name="WIDTH">Šířka hracího pole, pro nastavení počtu sloupců</param>
+        public Solver(int WIDTH)
+        {
+            columnOrder = new int[WIDTH];
+            bestCol = -1;
+
+            for (int i = 0; i < WIDTH; i++)
+            {
+                columnOrder[i] = WIDTH / 2 + (1 - 2 * (i % 2)) * (i + 1) / 2;         // Nastavuje pořadí sloupců, začíná se uprostřed a dále se alternuje levý a pravý sloupec
+            }
+        }
+
+
+        /// <summary>
+        /// Jednodužší verze MiniMaxu, která vyhodnotí skóre pozice a nejlepší tah
+        /// </summary>
+        /// <param name="P"></param>
+        /// <returns>skóre pozice, nejlepší tah</returns>
+        public (int, int) Negamax(Position P)                                                           // Tato funkce je zde zbytečná, slouží pouze jako prvotní verze finálního algoritmu
+        {
+            int bestCol = -1;           // Iniciace nejlepšího tahu, který vracíme
+
+            if (P.NbMoves() == P.WIDTH * P.HEIGHT)  // Kontrolujeme remízu
+                return (0, bestCol);                // Pokud nastává remíza, vracíme skóre 0 a nejlepší tah
+
+            for (int i = 0; i < P.WIDTH; i++)                                           // Kontrolujeme zda může hráč okamžitě vyhrát
+                if (P.CanPlay(i) && P.IsWinningMove(i, 1 + P.NbMoves() % 2))            // 1 + P.NbMoves() % 2 je symbol momentálně hrajícího hráče (buď 1 nebo 2)
+                    return ((P.WIDTH * P.HEIGHT + 1 - P.NbMoves()) / 2, bestCol);       // Vracíme skóre pozice a nejlepší tah
+
+            int bestScore = -P.WIDTH * P.HEIGHT;        // Nejlepší skóre nastavujeme na nejnižší možnou hodnotu
+
+            // Procházíme pole pro každý možný tah pustíme funkci Negamax rekurzivně
+            for (int x = 0; x < P.WIDTH; x++)
+            { 
+                if (P.CanPlay(x))                               // Kontrolujeme, jestli můžeme tah zahrát
+                {
+                    Position P2 = new Position(P);
+                    P2.Play(x);
+                    //P2.PrintBoard();
+                    int score = -Negamax(P2).Item1;             // Pouštíme rekurzivně na novou pozici se zahraným tahem
+                    
+                    // Uložíme nejlepší skóre a k němu příslušný sloupec
+                    if (score > bestScore)                  
+                    {
+                        bestScore = score;
+                        bestCol = x;
+                    }
+                }
+            }
+            return (bestScore, bestCol);
+        }
+
+
+        /// <summary>
+        /// Negamax s aplha-beta pruningem
+        /// </summary>
+        /// <param name="P">Pozice na prozkoumání</param>
+        /// <param name="alpha">Skóre minimálního nebo maximálního hráče (nvm moc tomu nerozumim)</param>
+        /// <param name="beta">To stejný jak alpha</param>
+        /// <returns>Skóre pozice</returns>
+        public (int, int) AlphaBeta(Position P, int alpha, int beta)
+        {
+            // Většina stejné jako v Negamaxu, již nekomentuji
+            int localBestCol = -1;
+
+            if (P.NbMoves() == P.WIDTH * P.HEIGHT) // Remíza
+                return (0, localBestCol);
+
+            for (int i = 0; i < P.WIDTH; i++) // Okamžitá výhra
+                if (P.CanPlay(i) && P.IsWinningMove(i, 1 + P.NbMoves() % 2))
+                    return ((P.WIDTH * P.HEIGHT + 1 - P.NbMoves()) / 2, i);
+
+            int max = (P.WIDTH * P.HEIGHT - 1 - P.NbMoves()) / 2;           // Nejlepší skóre nastavujeme na nejnižší možnou hodnotu
+
+            // Zmenšujeme interval prohledávání
+            if (beta > max)                             
+            {
+                beta = max;
+                if (alpha >= beta)
+                    return (beta, localBestCol);
+            }
+
+            for (int x = 0; x < P.WIDTH; x++)
+            {
+                int col = columnOrder[x];               // Procházíme sloupce odprostřed, jelikož je větší šance najít dobrý tah, tudíž využít pruning
+                if (P.CanPlay(col))
+                {
+                    Position P2 = new Position(P);
+                    P2.Play(col);
+                    int score = -AlphaBeta(P2, -beta, -alpha).Item1;
+
+                    // Tohle moc nechápu, ale prostě zmenšujeme ten interval prohledávání
+                    if (score >= beta)
+                        return (score, col);
+
+                    if (score > alpha)
+                    {
+                        alpha = score;
+                        localBestCol = col;
+                    }
+                }
+            }
+            return (alpha, localBestCol);
+        }
+
+
+        /// <summary>
+        /// Hloupý algoritmus na hraní AI
+        /// </summary>
+        /// <param name="P">Momentální stav hrací plochy</param>
+        /// <returns>true: zahraný tah byl výherní; false: zahraný tah nebyl výherní</returns>
+        public bool StupidAIPlay(Position P)
+        {
+            // Hledá výhru pro sebe
+            for (int i = 0; i < P.WIDTH; i++)
+            {
+                if (P.IsWinningMove(i, 1 + P.NbMoves() % 2))
+                {
+                    P.Play(i);
+                    P.PrintBoard();
+                    return true;
+                }
+            }
+
+            // Hraje aby zablokoval protihráčovy 3 kameny
+            // Je to kvůli tomu, aby alg nenašel jako první 2 sousední kameny a poté nechal hráče vyhrát někde kam nedošel
+            for (int i = 0; i < P.WIDTH; i++)
+            {
+                if (P.IsWinningMove(i, 2 - P.NbMoves() % 2))            
+                {                                                       
+                    P.Play(i);
+                    P.PrintBoard();
+                    P.winCount++;
+                    return false;
+                }
+            }
+
+            // Hraje aby zablokoval protihráčovy 2 kameny
+            P.winCount--;                       // Zmenšujeme winCount o 1, aby se bránílo už když má winCount-2 vedle sebe
+            for (int i = 0; i < P.WIDTH; i++)
+            {
+                if (P.IsWinningMove(i, 2 - P.NbMoves() % 2))            // Pokud má protihráč alespoň winCount-2 vedle sebe, tak ho bráníme
+                {                                                       // 2 - P.NbMoves() % 2 nastavuje kontrolovaného hráče na protihráče
+                    P.Play(i);
+                    P.PrintBoard();
+                    P.winCount++;
+                    return false;
+                }
+            }
+            P.winCount++;
+
+            // Hraje do sloupce s prioritou podle columnOrder
+            for (int x = 0; x < P.WIDTH; x++)
+            {
+                if (P.CanPlay(columnOrder[x]))
+                {
+                    P.Play(columnOrder[x]);
+                    P.PrintBoard();
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 7M/Hotove_ukoly/Connect4/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Connect4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (int height, int width, int winCount, int playerDecision) = Input();
+            Position P = new Position(height, width, winCount);
+
+            if (playerDecision == 1)
+                vsAI(P);
+            else
+                vsPlayer(P);
+
+            Console.WriteLine("Díky za zahrání!");
+        }
+
+        /// <summary>
+        /// Nahrání výšky a šířky hracího pole, počtu kamenů pro výhru a jestli chceme hrát proti AI či člověku
+        /// </summary>
+        /// <returns>Výška, šířka, winCount a proti komu chceme hrát</returns>
+        static (int, int, int, int) Input()
+        {
+            Console.WriteLine("Ahoj! Vítej ve hře Connect4.");
+            Console.WriteLine("Zadej výšku hracího pole: ");
+            if (!int.TryParse(Console.ReadLine(), out int height))
+                Console.WriteLine("Super, neumíš napsat číslo. Nehodlám sem kvůli tobě přidávat celej while cyklus, takže laskavě restartuj program a nauč se psát čísla.");
+            Console.WriteLine("Zadej šířku hracího pole: ");
+            if (!int.TryParse(Console.ReadLine(), out int width))
+                Console.WriteLine("Super, neumíš napsat číslo. Nehodlám sem kvůli tobě přidávat celej while cyklus, takže laskavě restartuj program a nauč se psát čísla.");
+            Console.WriteLine("Na kolik výherních kamenů chceš hrát?");
+            if (!int.TryParse(Console.ReadLine(), out int winCount))
+                Console.WriteLine("Super, neumíš napsat číslo. Nehodlám sem kvůli tobě přidávat celej while cyklus, takže laskavě restartuj program a nauč se psát čísla.");
+            Console.WriteLine("Chceš hrát proti AI (napiš 1) nebo hráči (napiš 2)?");
+            if (!int.TryParse(Console.ReadLine(), out int playerDecision))
+                Console.WriteLine("Super, neumíš napsat číslo. Nehodlám sem kvůli tobě přidávat celej while cyklus, takže laskavě restartuj program a nauč se psát čísla.");
+            return (height, width, winCount, playerDecision);
+        }
+
+
+        /// <summary>
+        /// Provede tah do sloupce, který napíšeme do konzole
+        /// </summary>
+        /// <param name="P">Momentální stav hracího pole</param>
+        /// <returns>true: hráč zahráním tohoto pole vyhrál; false: hra pokračuje bez výhry</returns>
+        static bool Turn(Position P)
+        {
+            // While pro vícero pokusů o napsání funkčního sloupce
+            while (true)
+            {
+                Console.Write("Zadej číslo sloupce: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int col))                  // Zkoušíme jestli je input integer
+                {
+                    Console.WriteLine("Zadej platné číslo sloupce");
+                    continue;
+                }
+                col--;
+                if (col < 0 || col >= P.WIDTH)                      // Zkoušíme jestli je input v hranicích pole
+                {
+                    Console.WriteLine("Zadej platné číslo sloupce");
+                    continue;
+                }
+                if (!P.CanPlay(col))                                // Zkoušíme jestli není chtěný sloupec plný
+                {
+                    Console.WriteLine("Toto pole je zabrané");
+                    continue;
+                }
+                if (P.IsWinningMove(col, 1 + P.NbMoves() % 2))      // Zkoušíme jestli tímto tahem hráč vyhraje
+                {                                                   // Pokud vyhraje, vracíme true
+                    P.Play(col);
+                    P.PrintBoard();
+                    return true;
+                }
+                // Zahrajeme tah
+                P.Play(col);
+                break;
+            }
+            // Vracíme false, jelikož tah byl proveden, ale nebyl výherní
+            P.PrintBoard();
+            return false;
+        }
+
+
+        /// <summary>
+        /// Hrajeme proti AI
+        /// </summary>
+        /// <param name="P">Počáteční pozice</param>
+        static void vsAI(Position P)
+        {
+            // Nahrání vstupů
+            Console.WriteLine("Zadej počet tahů, po kterém se bude používat Negamax. ");
+            Console.WriteLine("Pro normální velikost 7x6 doporučuji cca 16 tahů, poté musíš upravovat podle velikosti pole. (čím větší pole, tím déle to potrvá)");
+            if (!int.TryParse(Console.ReadLine(), out int movesCount))
+                Console.WriteLine("Super, neumíš napsat číslo. Nehodlám sem kvůli tobě přidávat celej while cyklus, takže laskavě restartuj program a nauč se psát čísla.");
+            Console.WriteLine("Chceš začínat ty nebo AI? (1 - já, 2 - AI)");
+            if (!int.TryParse(Console.ReadLine(), out int startDecision))
+                Console.WriteLine("Super, neumíš napsat číslo. Nehodlám sem kvůli tobě přidávat celej while cyklus, takže laskavě restartuj program a nauč se psát čísla.");
+
+            Console.WriteLine("Sloupce se číslují zleva od čísla 1.");
+
+            Solver solver = new Solver(P.WIDTH);        // Iniciace objektu solver třídy Solver
+            int bestCol;                                // Deklarování proměnné vracené funkcí AlphaBeta
+
+            P.PrintBoard();         // Tiskneme pole pro přehlednost pro uživatele
+
+            // Opakujeme, dokud někdo nevyhraje, nebo není remíza 
+            while (true)
+            {
+                // Check remízy
+                if (P.NbMoves() == P.WIDTH * P.HEIGHT)
+                {
+                    Console.WriteLine("Hra skončila remízou.");
+                    return;
+                }
+                //Console.WriteLine(P.NbMoves());
+
+                // Zjišťuje, jestli je na řadě člověk nebo AI. Záleží na tom kdo začíná
+                if (startDecision % 2 == 1)
+                {
+                    // Na tahu je člověk
+                    Console.WriteLine("Jseš na tahu");
+                    if (Turn(P))                                    // Provedeme tah a pokud je výherní, tak končíme
+                    {
+                        Console.WriteLine("Vyhrál jsi!");
+                        return;
+                    }
+                    startDecision++;            // Zvětšujeme pro posunutí tahu na AI
+                }
+                else
+                {
+                    // Na tahu je AI
+
+                    // Kontrolujeme, jestli je už odehráno dost tahů na zapnutí Alpha-beta pruningu Negamaxu. Pokud ho zapneme moc brzo, bude to trvat moc dlouho, jelikož složitost je exponenciální
+                    if (movesCount > P.NbMoves())
+                    {
+                        // Bylo zahráno moc málo tahů, takže pouštíme "hloupý" algoritmus
+                        if (solver.StupidAIPlay(P))
+                        {
+                            // Pokud AI zahrálo výherní tah, končíme
+                            Console.WriteLine("AI vyhrálo");
+                            return;
+                        }
+                        startDecision++;        // Zvětšujeme pro posunutí tahu na člověka
+                    }
+                    else
+                    {
+                        // Jsme dostatečně daleko ve hře, zapínáme zapínáme pásy!!!!
+                        bestCol = solver.AlphaBeta(P, -P.HEIGHT * P.WIDTH / 2, P.HEIGHT * P.WIDTH / 2).Item2;                   // Pustíme algoritmus a uložíme nejlepší tah
+
+                        // Jestliže je nejlepší tah výherní, končíme
+                        if (P.IsWinningMove(bestCol, 1 + P.NbMoves() % 2))
+                        {
+                            P.Play(bestCol);
+                            P.PrintBoard();
+                            Console.WriteLine("AI vyhrálo");
+                            return;
+                        }
+
+                        // Hrajeme tah a jde se dál
+                        P.Play(bestCol);
+                        P.PrintBoard();
+                        startDecision++;
+                    }
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// Hrajeme proti člověku
+        /// </summary>
+        /// <param name="P">Počáteční pozice</param>
+        static void vsPlayer(Position P)
+        {
+            // Načítáme jména hráčů
+            Console.WriteLine("Zadej jméno prvního hráče: ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Zadej jméno druhého hráče: ");
+            string name2 = Console.ReadLine();
+
+            Console.WriteLine("Sloupce se číslují zleva od čísla 1.");
+
+            P.PrintBoard();         // Tiskneme pole pro přehlednost pro uživatele
+
+            // Hrajeme dokud někdo nevyhraje, nebo hra neskončí remízou
+            while (true)
+            {
+                // Check remízy
+                if (P.NbMoves() == P.WIDTH * P.HEIGHT)
+                {
+                    Console.WriteLine("Hra skončila remízou.");
+                    return;
+                }
+
+                // Tah hráče 1
+                Console.WriteLine($"Na tahu je {name1}");
+                if (Turn(P))
+                {
+                    Console.WriteLine($"Vyhrál/a {name1}");
+                    return;
+                }
+
+                // Tah hráče 2
+                Console.WriteLine($"Na tahu je {name2}");
+                if (Turn(P))
+                {
+                    Console.WriteLine($"Vyhrál/a {name2}");
+                    return;
+                }
+            }
+        }
+    }
+}
