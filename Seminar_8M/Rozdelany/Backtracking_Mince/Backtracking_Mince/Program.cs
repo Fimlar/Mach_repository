a58885fe73c39316c4429bfb5d7104d981f455c5
Fimlar <filip@gaa.cz>
2026-03-02 09:28:28 +0100

    Mince Input

--- Seminar_8M/Rozdelany/Backtracking_Mince/Backtracking_Mince/Program.cs
+++ Seminar_8M/Rozdelany/Backtracking_Mince/Backtracking_Mince/Program.cs
@@ -10,6 +10,47 @@ namespace Backtracking_Mince
     {
         static void Main(string[] args)
         {
+            Input();
+        }
+
+        static void Input()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            List<int> numbers = new List<int>();
+
+            foreach (string part in input)
+            {
+                if (!int.TryParse(part, out int value))
+                {
+                    Console.WriteLine($"Chyba: '{part}' není platné číslo.");
+                    return;
+                }
+
+                if (value <= 0)
+                {
+                    Console.WriteLine($"Chyba: '{value}' není kladné číslo.");
+                    return;
+                }
+
+                numbers.Add(value);
+            }
+
+            numbers.Sort();
+            numbers.Reverse();
+
+            int n;
+            string x = Console.ReadLine();
+
+            if (!int.TryParse(x, out n))
+            {
+                Console.WriteLine("Zadej platné číslo");
+                return;
+            }
+            if (n<0)
+            {
+                Console.WriteLine("Zadej kladné číslo");
+                return;
+            }
         }
     }
 }
