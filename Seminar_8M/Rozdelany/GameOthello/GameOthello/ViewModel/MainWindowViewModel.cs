10da0e443e9e20f8a6c7456972409d640bcd1e12
Fimlar <filip@gaa.cz>
2026-03-23 16:03:38 +0100

    Othello workin'
    Othello funguje, ale nemá ještě ty fancy funkcionality

--- Seminar_8M/Rozdelany/GameOthello/GameOthello/ViewModel/MainWindowViewModel.cs
+++ Seminar_8M/Rozdelany/GameOthello/GameOthello/ViewModel/MainWindowViewModel.cs
@@ -4,9 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace GameOthello.ViewModel
@@ -45,9 +47,15 @@ namespace GameOthello.ViewModel
         /// </summary>
         public ObservableCollection<CellViewModel> Cells { get; } = new ObservableCollection<CellViewModel>();
 
-        private List<List<CellViewModel>> results = new List<List<CellViewModel>>();
+        /// <summary>
+        /// List cest k buňkám, které mohu zahrát
+        /// </summary>
+        private List<List<CellViewModel>> paths { get; set; } = new List<List<CellViewModel>>();
 
-        private List<CellViewModel> playables = new List<CellViewModel>();
+        /// <summary>
+        /// List buněk, které mohu zahrát
+        /// </summary>
+        private List<CellViewModel> playables { get; set; } = new List<CellViewModel>();
 
         /// <summary>
         /// Proměnná ukládající kdo je právě na řadě
@@ -90,10 +98,52 @@ namespace GameOthello.ViewModel
 
         private void CellClicked(CellViewModel cell)
         {
+            // Pokud hráč neklikl na buňky, která lze zahrát, rovnou se vracíme
+            if (cell.State != CellState.Playable)
+                return;
 
+            // Přidáme danou buňku hráči na řadě
+            cell.State = turn;
+            // Zjistíme index kliknuté buňky v listu playables
+            int index = playables.IndexOf(cell);
+            // Zkopíruji cestu příslušící kliknuté buňce
+            List<CellViewModel> currentPath = paths[index];
+            
+            // Všechny buňky v cestě dám momentálnímu hráči
+            foreach (CellViewModel path in currentPath)
+            {
+                path.State = turn;
+            }
+
+            // Změním tah na druhého hráče
+            switch (turn)
+            {
+                case CellState.FirstPlayer:
+                    turn = CellState.SecondPlayer;
+                    break;
+                case CellState.SecondPlayer: 
+                    turn = CellState.FirstPlayer;
+                    break;
+            }
+
+            foreach (var random in Cells.Where(c => c.State == CellState.Playable))
+            {
+                random.State = CellState.NotFound;
+            }
+
+            ColorPossibleMoves();
+        }
+        private void ColorPossibleMoves()
+        {
+            paths.Clear();
+            playables.Clear();
+            FindPossibleMoves();
+            // Nastavím všem možným buňkám stav na Playable
+            foreach (CellViewModel cell in playables)
+                cell.State = CellState.Playable;
         }
 
-        private void ColorPossibleMoves()
+        private void FindPossibleMoves()
         {
             foreach (var cell in Cells.Where(c => c.State == turn))
             {
@@ -105,38 +155,40 @@ namespace GameOthello.ViewModel
                     CellViewModel? nei = Cells.FirstOrDefault(c => c.Row == cell.Row + dy && c.Column == cell.Column + dx && c.State != CellState.NotFound &&
                                                                                                                    c.State != CellState.Playable &&
                                                                                                                    c.State != turn);
+                    // Pokud není vhodný soused v daném směru tak končím
+                    if (nei is null)
+                        continue;
+
                     // proměnná na posouvání ve směru
                     int i = 2;
                     // List, do kterého ukládám již projité buňky v tomto bloku
                     List<CellViewModel> temporary = new List<CellViewModel>();
-
-                    // Pokud buňka odpovídá, tak pokračuji v projíždění daným směrem
-                    if (nei != null)
+                    // Přidám do cesty právě prohledanou buňku
+                    temporary.Add(nei);
+                    while (true)
                     {
-                        while (true)
+                        // Najdu další buňku
+                        nei = Cells.FirstOrDefault(c => c.Row == cell.Row + i * dy && c.Column == cell.Column + i * dx);
+
+                        if (nei is null )
+                            break;
+                        // Pokud jsem narazil na prázdnou buňku, tak uložím konečnou buňky, uložím cestu k ní a skončím
+                        if (nei.State == CellState.NotFound)
                         {
-                            // Najdu další buňku
-                            nei = Cells.FirstOrDefault(c => c.Row == cell.Row + i*dy && c.Column == cell.Column + i*dx);
-                            
-                            // Pokud jsem narazil na prázdnou buňku, tak uložím projité buňky, uložím konečnou a skončím
-                            if (nei.State == CellState.NotFound)
-                            {
-                                playables.Add(nei);
-                                results.Add(temporary);
-                                break;
-                            }
-                            else if(nei.State == turn || nei.State == CellState.Playable)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                temporary.Add(nei);
-                                i++;
-                            }
+                            playables.Add(nei);
+                            paths.Add(new List<CellViewModel>(temporary));
+                            break;
+                        }
+                        // Pokud narazím na mojí buňku nebo buňku kterou už jsem našel, tak končím
+                        else if (nei.State == turn || nei.State == CellState.Playable)
+                            break;
+                        // Pokud narazím na cizí buňku, tak ji přidám do cesty
+                        else
+                        {
+                            temporary.Add(nei);
+                            i++;
                         }
                     }
-
                 }
             }
         }
